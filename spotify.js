303ccb9fc5d6536092c08f9d709a00cc9ec5c07a
Saniya Zehra <rizvisaniya055@gmail.com>
2025-05-08 17:31:11 +0530

    adding the files

---
+++ spotify.js
@@ -0,0 +1,5 @@
+let green1 = document.querySelectorAll(".card-artist-container");
+let grn1Hov = document.querySelectorAll(".green-artist-play");
+green1.addEventListener("hover" , ()=>{
+           grn1Hov.classList.add(".green-play-visible");
+})
